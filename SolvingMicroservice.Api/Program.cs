de32dcdacadd3ceafd2bc1679bdc9c03f1a6ed75
zitiret <xnncs@mail.ru>
2024-10-30 20:34:18 +0300

    Added SolvingMicroservice, Kafka basic architecture

---
+++ SolvingMicroservice.Api/Program.cs
@@ -0,0 +1,12 @@
+using SolvingMicroservice.Api.Extensions;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+IServiceCollection services = builder.Services;
+ConfigurationManager configuration = builder.Configuration;
+
+services.ConfigureDependencyInjection(configuration);
+
+WebApplication app = builder.Build();
+app.ConfigureHttpPipeline();
+
+app.Run();
